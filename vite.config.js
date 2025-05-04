a3ccf8e790962cc4f6f01d41c1fd0787c4e7ec64
Dimitar0902 <133356778+Dimitar0902@users.noreply.github.com>
2025-05-04 21:12:42 +0200

    Add built docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  // ← this must match your repo name
+  base: '/portfolio/',
+  plugins: [react()],
+  build: {
+    // output to docs/ so GitHub Pages can serve it
+    outDir: 'docs'
+  }
 })
